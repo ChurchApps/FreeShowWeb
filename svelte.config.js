e1c8884a1964ea04eefa73a6e23a0b0773719593
Kristoffer Vassbø <kristoffervassbo@gmail.com>
2022-09-08 17:09:59 +0200

    Actually working forms hopefully

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-auto"
+import preprocess from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
+  prerender: {
+    crawl: true,
+    enabled: true,
+    onError: "continue",
+    entries: ["*"],
+  },
+  kit: {
+    adapter: adapter(),
+  },
+}
 
-	kit: {
-		adapter: adapter()
-	}
-};
-
-export default config;
+export default config
