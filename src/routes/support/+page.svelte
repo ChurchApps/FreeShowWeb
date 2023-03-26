6f9e5c65b6a296dd0ec1bd157297c6cbb7353e4d
Kristoffer Vassbø <kristoffervassbo@gmail.com>
2023-03-26 16:28:29 +0200

    ☕ Added buymeacoffee

--- src/routes/support/+page.svelte
+++ src/routes/support/+page.svelte
@@ -39,6 +39,16 @@
                 <script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script>
             </span>
         </div>
+        <div>
+            <Button href="https://www.buymeacoffee.com/freeshow" target="_blank">
+                <h4 style:color="#FFDD00">-> buymeacoffee</h4>
+            </Button>
+            <span class="buymeacoffee">
+                <a href="https://www.buymeacoffee.com/freeshow" target="_blank" rel="noreferrer"
+                    ><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" /></a
+                >
+            </span>
+        </div>
     </div>
 </main>
 
