ac15c9b7c9283665fe2b3aa76840bd46e8d10994
Kristoffer Vassbø <kristoffervassbo@gmail.com>
2023-10-23 10:07:25 +0200

    Fixed prerender errors

--- src/routes/downloading/+page.svelte
+++ src/routes/downloading/+page.svelte
@@ -1,3 +1,7 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
 <script>
 	import Link from '$lib/components/inputs/Link.svelte';
 	import Section from '$lib/components/main/Section.svelte';
