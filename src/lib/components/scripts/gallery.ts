3f1e3490e639e5b1c875cab4c155cf7f5c341e99
Kristoffer <kristoffervassbo@gmail.com>
2025-07-18 18:07:40 +0200

    🖼️ Updated app images

--- src/lib/components/scripts/gallery.ts
+++ src/lib/components/scripts/gallery.ts
@@ -1,131 +1,129 @@
 export const images = [
 	{
-		src: 'preview/show.webp',
-		name: 'Show',
-		description:
-			'Create a show for lyrics, presentations, events and more. Arrange slides, and create different layouts for the same song.'
+		src: "preview/show.webp",
+		name: "Show",
+		description: "Create a show for lyrics, presentations, events and more. Arrange slides, and create different layouts for the same song."
 	},
 	{
-		src: 'preview/media.webp',
-		name: 'Media',
-		description:
-			'Show images and videos easily from the drawer. Drag them on your slides to set as background.'
+		src: "preview/media.webp",
+		name: "Media",
+		description: "Show images and videos easily from the drawer. Drag them on your slides to set as background."
 	},
 	{
-		src: 'preview/edit.webp',
-		name: 'Edit',
-		description:
-			'A powerful editor. Change the text font, size or color. Add outline and shadows, and much more. The possibilities are endless (almost)!'
+		src: "preview/edit.webp",
+		name: "Edit",
+		description: "A powerful editor. Change the text font, size or color. Add outline and shadows, and much more. The possibilities are endless (almost)!"
 	},
 	{
-		src: 'preview/autoplay.webp',
-		name: 'Autoplay',
-		description: 'Set timer on slides to continue to next.'
+		src: "preview/autoplay.webp",
+		name: "Autoplay",
+		description: "Set timer on slides to continue to next."
 	},
 	{
-		src: 'preview/timers.webp',
-		name: 'Timer',
-		description: 'Add countdowns or a timer.'
+		src: "preview/timers.webp",
+		name: "Timer",
+		description: "Add countdowns or a timer."
 	},
 	{
-		src: 'preview/show_2.webp',
-		name: 'Layout',
-		description: 'Resize different areas after your needs.'
+		src: "preview/show_2.webp",
+		name: "Layout",
+		description: "Resize different areas after your needs."
 	},
 	{
-		src: 'preview/player_youtube.webp',
-		name: 'YouTube player',
-		description:
-			'No more dragging windows around or downloading videos. Just paste the link and play the video.'
+		src: "preview/youtube.webp",
+		name: "YouTube player",
+		description: "No more dragging windows around or downloading videos. Just paste the link and play the video."
 	},
 	{
-		src: 'preview/player_vimeo.webp',
-		name: 'Vimeo player',
-		description: 'Same as YouTube player, just for Vimeo videos.'
+		src: "preview/vimeo.webp",
+		name: "Vimeo player",
+		description: "Same as YouTube player, just for Vimeo videos."
 	},
 	{
-		src: 'preview/show_edit.webp',
-		name: 'Edit mode',
-		description: 'Edit text on the fly with this mode.'
+		src: "preview/show_edit.webp",
+		name: "Edit mode",
+		description: "Edit text on the fly with this mode."
 	},
 	{
-		src: 'preview/show_lyrics.webp',
-		name: 'Lyrics mode',
-		description: 'Sing along with the lyrics mode.'
+		src: "preview/show_lyrics.webp",
+		name: "Lyrics mode",
+		description: "Sing along with the lyrics mode."
 	},
 	{
-		src: 'preview/show_text.webp',
-		name: 'Text mode',
-		description: 'Easy copy/paste using text mode.'
+		src: "preview/show_text.webp",
+		name: "Text mode",
+		description: "Easy copy/paste using text mode."
 	},
 	{
-		src: 'preview/templates.webp',
-		name: 'Templates',
-		description: 'Change the style of your slides with the click of a button.'
+		src: "preview/templates.webp",
+		name: "Templates",
+		description: "Change the style of your slides with the click of a button."
 	},
 	{
-		src: 'preview/stage.webp',
-		name: 'Stage',
-		description:
-			'Connect from any local device, by entering the IP and a set port. Create and edit multiple stage views.'
+		src: "preview/overlays.webp",
+		name: "Overlays",
+		description: "Play overlays that are placed on top of other content. Set a timeout to automatically hide again."
 	},
 	{
-		src: 'preview/draw.webp',
-		name: 'Draw',
-		description:
-			'Shine a light on your slides while presenting. Focus the audience, play with particles, or draw a beautiful drawing.'
+		src: "preview/stage.webp",
+		name: "Stage",
+		description: "Connect from any local device, by entering the IP and a set port. Create and edit multiple stage views."
 	},
 	{
-		src: 'preview/calendar.webp',
-		name: 'Calendar',
-		description:
-			'A calendar for all your events. Select them and create a show with info about upcomming events. Or schedule actions to run automatically.'
+		src: "preview/draw.webp",
+		name: "Draw",
+		description: "Shine a light on your slides while presenting. Focus the audience, play with particles, or draw a beautiful drawing."
+	},
+	// {
+	// 	src: "preview/calendar.webp",
+	// 	name: "Calendar",
+	// 	description: "A calendar for all your events. Select them and create a show with info about upcomming events. Or schedule actions to run automatically."
+	// },
+	{
+		src: "preview/scripture.webp",
+		name: "Scripture",
+		description: "Import your own local bibles, or choose from over 200 bibles provided by API.Bible."
 	},
 	{
-		src: 'preview/scripture.webp',
-		name: 'Scripture',
-		description:
-			'Import your own local bibles, or choose from over 200 bibles provided by API.Bible.'
+		src: "preview/output.webp",
+		name: "Settings",
+		description: "Customize and manage different outputs."
 	},
 	{
-		src: 'preview/output.webp',
-		name: 'Settings',
-		description: 'Customize and manage different outputs.'
+		src: "preview/outputs.webp",
+		name: "Multiple outputs",
+		description: "Easy control of multiple different outputs."
 	},
-	{
-		src: 'preview/outputs.webp',
-		name: 'Multiple outputs',
-		description: 'Easy control of multiple different outputs.'
-	},
+	// old
 	// {
 	//     src: "preview/themes_0.webp",
 	//     name: "Themes",
 	//     description: "Create you own themes, tweak the colors and UI to the extreme.",
 	// },
 	{
-		src: 'preview/themes_1.webp',
-		name: '',
-		description: ''
+		src: "preview/themes_1.webp",
+		name: "",
+		description: ""
 	},
 	{
-		src: 'preview/themes_2.webp',
-		name: '',
-		description: ''
+		src: "preview/themes_2.webp",
+		name: "",
+		description: ""
 	},
 	{
-		src: 'preview/themes_3.webp',
-		name: '',
-		description: ''
+		src: "preview/themes_3.webp",
+		name: "",
+		description: ""
 	},
 	{
-		src: 'preview/themes_4.webp',
-		name: '',
-		description: ''
+		src: "preview/themes_4.webp",
+		name: "",
+		description: ""
 	},
 	{
-		src: 'preview/themes_5.webp',
-		name: '',
-		description: ''
+		src: "preview/themes_5.webp",
+		name: "",
+		description: ""
 	},
 	// {
 	//     src: "preview/themes_6.webp",
@@ -133,44 +131,44 @@ export const images = [
 	//     description: "",
 	// },
 	{
-		src: 'preview/themes_7.webp',
-		name: '',
-		description: ''
+		src: "preview/themes_7.webp",
+		name: "",
+		description: ""
 	},
 	{
-		src: 'preview/themes_8.webp',
-		name: '',
-		description: ''
+		src: "preview/themes_8.webp",
+		name: "",
+		description: ""
 	},
 	{
-		src: 'preview/themes_9.webp',
-		name: '',
-		description: ''
+		src: "preview/themes_9.webp",
+		name: "",
+		description: ""
 	},
 	{
-		src: 'preview/themes_10.webp',
-		name: '',
-		description: ''
+		src: "preview/themes_10.webp",
+		name: "",
+		description: ""
+	},
+	// {
+	// 	src: 'preview/history.webp',
+	// 	name: 'History',
+	// 	description: 'See all of the changes made, and revert them.'
+	// },
+	{
+		src: "preview/import.webp",
+		name: "Import",
+		description: "Import all your old songs."
 	},
 	{
-		src: 'preview/history.webp',
-		name: 'History',
-		description: 'See all of the changes made, and revert them.'
+		src: "preview/export.webp",
+		name: "Export",
+		description: "Export as PDF or another format."
 	},
 	{
-		src: 'preview/import.webp',
-		name: 'Import',
-		description: 'Import all your old songs.'
-	},
-	{
-		src: 'preview/export.webp',
-		name: 'Export',
-		description: 'Export as PDF or another format.'
-	},
-	{
-		src: 'preview/remote_show.webp',
-		name: 'Remote',
-		description: 'Connect from any local device, by entering the IP and a set port.'
+		src: "preview/remote_show.webp",
+		name: "Remote",
+		description: "Connect from any local device, by entering the IP and a set port."
 	},
 	// {
 	//     src: "preview/remote_preview.webp",
@@ -182,19 +180,20 @@ export const images = [
 	//     name: "Remote projects",
 	//     description: "",
 	// },
+	// {
+	// 	src: "preview/remote_lyrics.webp",
+	// 	name: "Lyrics view",
+	// 	description: "Different modes for easy control."
+	// },
 	{
-		src: 'preview/remote_lyrics.webp',
-		name: 'Lyrics view',
-		description: 'Different modes for easy control.'
-	},
-	{
-		src: 'preview/controller.webp',
-		name: 'Controller',
-		description: 'Easy presentation controller for anybody to use.'
+		// old
+		src: "preview/controller.webp",
+		name: "Controller",
+		description: "Easy presentation controller for anybody to use."
 	}
 	// {
 	//     src: "preview/stage_view.webp",
 	//     name: "Stage",
 	//     description: "Connect to stage from any webbrowser.",
 	// },
-];
+]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-	import Link from '$lib/components/inputs/Link.svelte';
-	import Card from '$lib/components/main/Card.svelte';
-	import Feature from '$lib/components/main/Feature.svelte';
-	import Icon from '$lib/components/main/Icon.svelte';
-	import Image from '$lib/components/main/Image.svelte';
-	import Section from '$lib/components/main/Section.svelte';
-	import Video from '$lib/components/main/Video.svelte';
-	import Badges from '$lib/components/special/Badges.svelte';
-	import Compare from '$lib/components/special/Compare.svelte';
-	import Download from '$lib/components/special/Download.svelte';
-	import Reviews from '$lib/components/special/Reviews.svelte';
+	import Link from "$lib/components/inputs/Link.svelte"
+	import Card from "$lib/components/main/Card.svelte"
+	import Feature from "$lib/components/main/Feature.svelte"
+	import Icon from "$lib/components/main/Icon.svelte"
+	import Image from "$lib/components/main/Image.svelte"
+	import Section from "$lib/components/main/Section.svelte"
+	import Video from "$lib/components/main/Video.svelte"
+	import Badges from "$lib/components/special/Badges.svelte"
+	import Compare from "$lib/components/special/Compare.svelte"
+	import Download from "$lib/components/special/Download.svelte"
+	import Reviews from "$lib/components/special/Reviews.svelte"
 </script>
 
 <Section
 	height={95}
 	background={{
-		src: 'main/header-bg.webp',
-		alt: 'Header image - person using FreeShow at concert'
+		src: "main/header-bg.webp",
+		alt: "Header image - person using FreeShow at concert"
 	}}
 	continueButton
 >
@@ -26,9 +26,7 @@
 			<span>Presentations</span>
 			<span>with FreeShow</span>
 		</h1>
-		<p style="padding: 15px 0;max-width: 370px;">
-			A dynamic, user-friendly, and open-source presenter built for all of your presentations.
-		</p>
+		<p style="padding: 15px 0;max-width: 370px;">A dynamic, user-friendly, and open-source presenter built for all of your presentations.</p>
 
 		<div class="download">
 			<Download main />
@@ -51,16 +49,13 @@
 		</div>
 
 		<p>
-			Every year churches spend hundreds of millions of dollars on the software needed to operate
-			effectively and efficiently. We believe that within the Church the talent exists to create all
-			the software that churches need and <strong>provide it free of charge.</strong>
+			Every year churches spend hundreds of millions of dollars on the software needed to operate effectively and efficiently. We believe that within the Church the talent exists to create all the software that churches need and <strong
+				>provide it free of charge.</strong
+			>
 		</p>
 		<p>
-			Changing how we handle church software can not only save a lot of money, but also bring
-			believers together. If we use the skills within our own community, we can make the software
-			that the Church needs without the costly expense. This way, more of our funds can go directly
-			to the Church's mission instead of overhead. It's about working together, saving money, and
-			focusing on what truly matters.
+			Changing how we handle church software can not only save a lot of money, but also bring believers together. If we use the skills within our own community, we can make the software that the Church needs without the costly expense. This
+			way, more of our funds can go directly to the Church's mission instead of overhead. It's about working together, saving money, and focusing on what truly matters.
 		</p>
 
 		<Link link="https://livecs.org/" title="Learn more about our mission" primary>Learn More</Link>
@@ -83,22 +78,12 @@
 		</p>
 	</div>
 
-	<div
-		class="center"
-		style="position: relative;width: var(--full-small-80-big);display: flex;justify-content: center;"
-	>
-		<Card
-			outline
-			glass
-			style="overflow: initial;z-index: 1;--primary: rgb(var(--primary-inverted-rgb), 0.6)"
-		>
+	<div class="center" style="position: relative;width: var(--full-small-80-big);display: flex;justify-content: center;">
+		<Card outline glass style="overflow: initial;z-index: 1;--primary: rgb(var(--primary-inverted-rgb), 0.6)">
 			<Video src="Introducing FreeShow.mp4" thumbnail="Introducing FreeShow.webp" />
 		</Card>
 
-		<div
-			class="circle"
-			style="position: absolute;top: -40px;left: -40px;width: 120px;opacity: 0.7;"
-		>
+		<div class="circle" style="position: absolute;top: -40px;left: -40px;width: 120px;opacity: 0.7;">
 			<Image src="special/circle-bg.webp" alt="Design circle" />
 		</div>
 	</div>
@@ -108,8 +93,8 @@
 	column
 	style="z-index: 0;"
 	background={{
-		src: 'main/features-bg-affix.webp',
-		alt: 'Distant background of computer running FreeShow',
+		src: "main/features-bg-affix.webp",
+		alt: "Distant background of computer running FreeShow",
 		top: -120
 	}}
 >
@@ -121,13 +106,8 @@
 	<div class="row" style="flex-direction: var(--inverted-column-small-row-big);">
 		<div class="text" style="align-self: center;">
 			<h4>Powerful Editing</h4>
-			<Feature icon="options" title="Limitless Customization">
-				A Robust editor to alter text font, size, color, and add outlines, shadows, with almost
-				boundless possibilities.
-			</Feature>
-			<Feature icon="edit" title="On-the-Fly Text Editing">
-				Make live changes to your text during presentations with the Edit mode.
-			</Feature>
+			<Feature icon="options" title="Limitless Customization">A Robust editor to alter text font, size, color, and add outlines, shadows, with almost boundless possibilities.</Feature>
+			<Feature icon="edit" title="On-the-Fly Text Editing">Make live changes to your text during presentations with the Edit mode.</Feature>
 		</div>
 
 		<div class="image">
@@ -147,61 +127,30 @@
 		<div class="text" style="align-self: center;">
 			<h4>Timed & Automated Play</h4>
 			<Feature icon="play" title="Autoplay">Set timers on slides for seamless transitions.</Feature>
-			<Feature icon="timer" title="Countdowns">
-				Easily let your congregation know when services will start with built-in countdowns.
-			</Feature>
-			<Feature icon="midi" title="MIDI Controls">
-				Receive MIDI commands to advance slides or send MIDI commands on slide change to trigger
-				lighting and other changes.
-			</Feature>
+			<Feature icon="timer" title="Countdowns">Easily let your congregation know when services will start with built-in countdowns.</Feature>
+			<Feature icon="midi" title="MIDI Controls">Receive MIDI commands to advance slides or send MIDI commands on slide change to trigger lighting and other changes.</Feature>
 		</div>
 	</div>
 </Section>
 
 <Section column style="padding: 0;gap: 0;">
-	<Image
-		src="special/flow-top.webp"
-		alt="Esthetic waves start"
-		draggable={false}
-		borderRadius={false}
-	/>
+	<Image src="special/flow-top.webp" alt="Esthetic waves start" draggable={false} borderRadius={false} />
 
 	<div class="row" style="padding: 0 var(--margin);background-color: #980057;">
 		<div class="text" style="align-self: center;padding: 30px 0;">
 			<h4 style="text-align: var(--center-small-left-big);width: 100%;">Multiple Views</h4>
-			<Feature icon="person" title="Simultaneous Outputs" white>
-				Show the same presentation with different looks for your in-person audience, online
-				audience, and a stage view for leaders.
-			</Feature>
-			<Feature icon="tv" title="Web Output" white>
-				Users can connect to the presentation from their mobile devices. Perfect for pastor sermon
-				notes or musicians.
-			</Feature>
-			<Feature icon="network" title="NDI Support" white>
-				We fully support output over NDI. No need for converters or SDI cables to send your lyrics
-				to the computer running your online stream.
-			</Feature>
-			<Feature icon="mobile" title="Remote Control" white>
-				Allow your pastor or worship minister to control the show directly from their mobile device.
-				Excellent for rehearsals or for smaller churches.
-			</Feature>
+			<Feature icon="person" title="Simultaneous Outputs" white>Show the same presentation with different looks for your in-person audience, online audience, and a stage view for leaders.</Feature>
+			<Feature icon="tv" title="Web Output" white>Users can connect to the presentation from their mobile devices. Perfect for pastor sermon notes or musicians.</Feature>
+			<Feature icon="network" title="NDI Support" white>We fully support output over NDI. No need for converters or SDI cables to send your lyrics to the computer running your online stream.</Feature>
+			<Feature icon="mobile" title="Remote Control" white>Allow your pastor or worship minister to control the show directly from their mobile device. Excellent for rehearsals or for smaller churches.</Feature>
 		</div>
 
 		<div class="image" style="filter: brightness(0.9);padding-top: 50px;">
-			<Image
-				src="special/devices.webp"
-				alt="Person working with FreeShow"
-				style="height: initial;"
-			/>
+			<Image src="special/devices.webp" alt="Person working with FreeShow" style="height: initial;" />
 		</div>
 	</div>
 
-	<Image
-		src="special/flow-bottom.webp"
-		alt="Esthetic waves end"
-		draggable={false}
-		borderRadius={false}
-	/>
+	<Image src="special/flow-bottom.webp" alt="Esthetic waves end" draggable={false} borderRadius={false} />
 </Section>
 
 <Section column style="gap: var(--gap);padding-bottom: calc(var(--section-padding) / 2);">
@@ -210,110 +159,52 @@
 	</div>
 
 	<div class="row">
-		<Card
-			outline
-			variant
-			bubble={1}
-			style="--card-padding: var(--card-padding-big);width: var(--full-small-half-big);"
-		>
-			<div
-				class="image"
-				style="margin: calc((var(--card-padding) - var(--outline-width)) * -1) calc((var(--card-padding) - var(--outline-width)) * -1) var(--card-padding) 0;"
-			>
-				<Image
-					src="preview/main_template.webp"
-					alt="Preview of software. Drawer with templates open."
-				/>
+		<Card outline variant bubble={1} style="--card-padding: var(--card-padding-big);width: var(--full-small-half-big);">
+			<div class="image" style="margin: calc((var(--card-padding) - var(--outline-width)) * -1) calc((var(--card-padding) - var(--outline-width)) * -1) var(--card-padding) 0;">
+				<Image src="preview/main_template.webp" alt="Preview of software. Drawer with templates open." />
 			</div>
 
 			<div style="display: flex;flex-direction: column;gap: 25px;position: relative;z-index: 1;">
 				<h4>Template & Design</h4>
-				<Feature icon="template" title="Sleek Templates">
-					Modify your slide styles swiftly with a variety of templates.
-				</Feature>
-				<Feature icon="layout" title="Dynamic Layouts">
-					Resize and organize your display areas as per your requirement.
-				</Feature>
+				<Feature icon="template" title="Sleek Templates">Modify your slide styles swiftly with a variety of templates.</Feature>
+				<Feature icon="layout" title="Dynamic Layouts">Resize and organize your display areas as per your requirement.</Feature>
 			</div>
 		</Card>
 
-		<Card
-			outline
-			bubble={2}
-			style="--card-padding: var(--card-padding-big);width: var(--full-small-half-big);"
-		>
+		<Card outline bubble={2} style="--card-padding: var(--card-padding-big);width: var(--full-small-half-big);">
 			<div style="display: flex;flex-direction: column;gap: 25px;position: relative;z-index: 1;">
 				<h4>Create & Show</h4>
-				<Feature icon="magic" title="Create Impactful Shows">
-					Easily arrange slides and layouts for lyrics, presentations, events, and more, ensuring a
-					smooth flow in your presentations.
-				</Feature>
-				<Feature icon="image" title="Dynamic Media Integration">
-					Effortlessly drag images and videos into your slides as backgrounds, enhancing the visual
-					appeal of your presentations.
-				</Feature>
+				<Feature icon="magic" title="Create Impactful Shows">Easily arrange slides and layouts for lyrics, presentations, events, and more, ensuring a smooth flow in your presentations.</Feature>
+				<Feature icon="image" title="Dynamic Media Integration">Effortlessly drag images and videos into your slides as backgrounds, enhancing the visual appeal of your presentations.</Feature>
 			</div>
 
-			<div
-				class="image"
-				style="margin: var(--card-padding) 0 calc((var(--card-padding) - var(--outline-width)) * -1) 0;"
-			>
-				<Image
-					src="preview/main_create.webp"
-					alt="Preview of software. Drawer with templates open."
-				/>
+			<div class="image" style="margin: var(--card-padding) 0 calc((var(--card-padding) - var(--outline-width)) * -1) 0;">
+				<Image src="preview/main_create.webp" alt="Preview of software. Drawer with templates open." />
 			</div>
 		</Card>
 	</div>
 
-	<Card
-		outline
-		bubble={3}
-		style="--card-padding: var(--card-padding-big);display: flex;flex-direction: var(--column-small-row-big);width: 100%;"
-	>
-		<div
-			class="image"
-			style="z-index: 1;margin: calc((var(--card-padding) - var(--outline-width)) * -1) var(--card-padding) var(--card-padding) calc((var(--card-padding) - var(--outline-width)) * -1)"
-		>
-			<Image
-				src="preview/main_scripture.webp"
-				alt="Preview of software. Drawer with templates open."
-			/>
+	<Card outline bubble={3} style="--card-padding: var(--card-padding-big);display: flex;flex-direction: var(--column-small-row-big);width: 100%;">
+		<div class="image" style="z-index: 1;margin: calc((var(--card-padding) - var(--outline-width)) * -1) var(--card-padding) var(--card-padding) calc((var(--card-padding) - var(--outline-width)) * -1)">
+			<Image src="preview/main_scripture.webp" alt="Preview of software. Drawer with templates open." />
 		</div>
 
 		<div class="text" style="align-self: center;z-index: 1;">
 			<h4>Scripture & Lyric Integration</h4>
-			<Feature icon="bible" title="Scripture Integration">
-				Easily look up passages and turn them into slides with one click.
-			</Feature>
-			<Feature icon="music" title="Lyric Integration">
-				It's no longer necessary to manually enter the lyrics to all the songs. Just enter the title
-				and click search and be amazed as the slides are automatically created for you.
-			</Feature>
+			<Feature icon="bible" title="Scripture Integration">Easily look up passages and turn them into slides with one click.</Feature>
+			<Feature icon="music" title="Lyric Integration">It's no longer necessary to manually enter the lyrics to all the songs. Just enter the title and click search and be amazed as the slides are automatically created for you.</Feature>
 		</div>
 	</Card>
 </Section>
 
 <!-- this should be a different image -->
-<Section
-	column
-	background={{ src: 'main/working.webp', alt: 'Distant background of person working on FreeShow' }}
-	style="padding-top: calc(var(--section-padding) / 2);"
->
+<Section column background={{ src: "main/working.webp", alt: "Distant background of person working on FreeShow" }} style="padding-top: calc(var(--section-padding) / 2);">
 	<div class="row" style="flex-direction: var(--inverted-column-small-row-big);">
 		<div class="text" style="align-self: center;">
 			<h4>Plays Well With Others</h4>
-			<Feature icon="import" title="Import">
-				Import Presentations from PowerPoint, ProPresenter, EasyWorship, OpenSong, and other
-				platforms.
-			</Feature>
-			<Feature icon="export" title="Export">
-				You're not locked in and can export your content in multiple formats at any time.
-			</Feature>
-			<Feature icon="cloud" title="Cloud Sync">
-				Allow multiple people within your church to work on presentations from different computers
-				with cloud sync.
-			</Feature>
+			<Feature icon="import" title="Import">Import Presentations from PowerPoint, ProPresenter, EasyWorship, OpenSong, and other platforms.</Feature>
+			<Feature icon="export" title="Export">You're not locked in and can export your content in multiple formats at any time.</Feature>
+			<Feature icon="cloud" title="Cloud Sync">Allow multiple people within your church to work on presentations from different computers with cloud sync.</Feature>
 		</div>
 
 		<div class="image">
@@ -336,32 +227,22 @@
 		</div>
 
 		<p style="width: 100%;">
-			Unlike other presentation software providers, we are not a business. We are a non-profit that
-			exists solely to serve churches. Our biggest difference is in the name. FreeShow is and always
-			will be free. This is possible due to the generous support of donors.
+			Unlike other presentation software providers, we are not a business. We are a non-profit that exists solely to serve churches. Our biggest difference is in the name. FreeShow is and always will be free. This is possible due to the
+			generous support of donors.
 			<a href="https://churchapps.org/contribute" target="_blank">Learn more.</a>
-			That isn't the only way FreeShow stands above the alternatives. It is also a very feature-rich
-			application. Here is how we stack up.
+			That isn't the only way FreeShow stands above the alternatives. It is also a very feature-rich application. Here is how we stack up.
 		</p>
 	</div>
 
 	<div class="compare" style="position: relative;">
 		<Compare maxItems={14} />
 
-		<div
-			class="circle"
-			style="position: absolute;bottom: -45px;right: -45px;width: 120px;opacity: 0.7;"
-		>
+		<div class="circle" style="position: absolute;bottom: -45px;right: -45px;width: 120px;opacity: 0.7;">
 			<Image src="special/circle-bg.webp" alt="Design circle" />
 		</div>
 	</div>
 
-	<Link
-		link="/features"
-		title="See more features of this program"
-		primary
-		style="align-self: center;">View All</Link
-	>
+	<Link link="/features" title="See more features of this program" primary style="align-self: center;">View All</Link>
 </Section>
 
 <Section style="padding-bottom: calc(var(--section-padding) / 2);" center>
@@ -412,6 +293,10 @@
 	@media only screen and (min-width: 400px) {
 		.top-content {
 			padding-top: 10px;
+		}
+
+		.image {
+			max-width: 50%;
 		}
 	}
 </style>
