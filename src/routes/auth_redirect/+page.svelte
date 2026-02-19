4420009dbb68ab7f029bd123e088939aca974148
Kristoffer <kristoffervassbo@gmail.com>
2026-02-19 18:10:17 +0100

    Auth redirect

---
+++ src/routes/auth_redirect/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { onMount } from "svelte"
+
+	onMount(() => {
+		const urlParams = new URLSearchParams(window.location.search)
+		const authCode = urlParams.get("code")
+
+		if (authCode) window.location.href = `http://127.0.0.1:5520/auth_redirect?code=${authCode}`
+		else window.location.replace("https://freeshow.app")
+	})
+</script>
+
+<h1>Redirecting...</h1>
+<p>If you are not redirected automatically, please try another browser or check your browser settings.</p>
